90a87c41e95e80a5bb6bedce058fef9249fe0e86
GenericLasagna <eliannuno@gmail.com>
2025-04-13 14:56:52 -0700

    allows for the creating of multiple sizes of matrix

--- matrix.java
+++ matrix.java
@@ -4,23 +4,18 @@ public class matrix {
     float [][] values;
 
     public static void main(String[] args) {
-        matrix m1 = new matrix();
+        matrix m1 = new matrix(3,3);
 
         modifyMatrix(m1, 0, 0 ,1f);
-        modifyMatrix(m1, 0, 3 ,4f);
-        modifyMatrix(m1, 1, 3 ,5f);
-        modifyMatrix(m1, 2, 1 ,7.5f);
-        modifyMatrix(m1, 3, 2 ,1f);
-
         
         showMatrix(m1);
 
     }
 
-    matrix(){
-        values = new float[4][4];
-        for (int i = 0; i<4; i++){
-            for(int j = 0; j<4;j++){
+    matrix(int x, int y){
+        values = new float[x][y];
+        for (int i = 0; i<x; i++){
+            for(int j = 0; j<y;j++){
               values[i][j] = 0f;  
             }
             
@@ -34,10 +29,10 @@ public class matrix {
     }
 
     public static void showMatrix(matrix m) {
-        for (int i = 0; i<4; i++){
+        for (int i = 0; i<m.values.length; i++){
             System.out.print("\n");
-            for(int j = 0; j<4;j++){
-              System.out.print(m.values[i][j]+" "); 
+            for(int j = 0; j<m.values[0].length;j++){
+              System.out.print(m.values[i][j]+"|"); 
             }
             
         }
